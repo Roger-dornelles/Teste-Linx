f6059f15c89f3a7509cf0a1283b01478eea7e48a
Roger <rsd.santo@outlook.com>
2021-07-16 15:36:38 -0300

    Atualização da requisição a API e validação de formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,50 +3,47 @@
     let url = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1`
     let req = await fetch(url);
     let json = await req.json();
-    console.log(json.products);
 
-    assembleList(json.products);
+    assembleList(json);
 }
 products();
-function assembleList(list) {
-    
-    let card = document.querySelector('.card');
-    card.cloneNode(true);
+function assembleList(lista) {
+
+    let list  = lista.products;//receber os produtos
+
+    //listar os produtos
     for(let i in list) {
-        card.querySelector('#description').innerHTML = list[i].description;
-        card.querySelector('img').src = list[i].image;
-        card.querySelector('#name').innerHTML= list[i].name;
-        card.querySelector('#oldPrice').innerHTML = `de R$: ${list[i].oldPrice.toFixed(2)}`;
-        card.querySelector('#price').innerHTML =`Por R$ ${list[i].price.toFixed(2)}`;
-        card.querySelector('#division').innerHTML = `ou 2X de ${(list[i].price / 2).toFixed(2)}`;
+        let card = document.querySelector('.models .card').cloneNode(true);
+        card.querySelector('.description_c').innerHTML = list[i].description;
+        card.querySelector('img').src = 'https:'+list[i].image;
+        card.querySelector('.name_c').innerHTML= list[i].name;
+        card.querySelector('.oldPrice_c').innerHTML = `de R$: ${list[i].oldPrice.toFixed(2)}`;
+        card.querySelector('.price_c').innerHTML =`Por R$ ${list[i].price.toFixed(2)}`;
+        card.querySelector('.division_c').innerHTML = `ou 2X de ${(list[i].price / 2).toFixed(2)}`;
 
-        console.log('DESC : ',list[i])
 
+        document.querySelector('.container').append(card);
 
     }
-    document.querySelector('.container').append(card);
-
-
+    
 }
-
 
 
 // (compartilhe a novidade) 
 // validação de campo email  ( SECTION SHARE-INFO )
-function validator(){
-   
-    let input = document.querySelector('#email').value;
-    if(input.value !== ''){
-        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
-        if(!regex.test(input)){
-            document.querySelector('.warning').style.display="flex";
+function validator(e){
+   e.preventDefault();
+    let email = document.querySelector('.email-form').value;
+  
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(!regex.test(email)){
+        document.getElementById('warning').style.display="flex";
 
-            setTimeout(()=>{
-                document.querySelector('.warning').style.display="none";
-            },1750);
-        }
-        
+        setTimeout(()=>{
+            document.getElementById('warning').style.display="none";
+        },1750);
     }
-    console.log("Input",input)
+        
+    
 };
-
